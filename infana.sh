fb05fe782551b9edbfac809b31fa269b3b96e5f8
Punit Sharma <punit15sharma@gmail.com>
2021-06-03 12:48:44 -0400

    Update infana.sh

--- infana.sh
+++ infana.sh
@@ -13,6 +13,14 @@ sudo systemctl unmask influxdb.service
 sudo systemctl start influxdb
 sudo systemctl enable influxdb.service
 
+read -sp 'Password for influx user grafana: ' passvar
+
+influx -execute 'create database Coldjig'
+influx -execute 'use Coldjig'
+influx -execute 'create user grafana with password '$passvar' with all privileges
+influx -execute 'grant all privileges on SHT31 to grafana'
+
+
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
 
@@ -22,5 +30,23 @@ sudo systemctl unmask grafana-server.service
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server.service
 
+curl https://pyenv.run | bash
 
-curl -L https://raw.githubusercontent.com/headmelted/codebuilds/master/docs/installers/apt.sh | sudo bash
+sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
+
+
+source ~/.bashrc
+pyenv install 3.8.0
+pyenv global 3.8.0
+
+sudo apt-get install pipenv
+pip install pipenv
